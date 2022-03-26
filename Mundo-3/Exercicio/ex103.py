9d97a38d33cfcc77a2a8eebe88ba5d05d6b7870c
Lucas Silva <LucasSilvaCode@gmail.com>
2022-03-26 19:35:19 -0300

    master

--- Mundo-3/Exercicio/ex103.py
+++ Mundo-3/Exercicio/ex103.py
@@ -1,15 +1,14 @@
 '''Faça um programa que tenha uma funçao chamada ficha(), que receba o nome de um jogador e quantos gols ele marcou
 O programa deverá ser capas de mostrar a ficha do jogador, mesmo que algum dado não tenha cido informado. '''
 
-def ficha (nome, gols=0):
-    nome = '<desconhecido>'
+def ficha (nome = '<desconhecido>', gols=0):
     print('~-'*30)
-    nome = str(input('Nome do jogador:'))
-    gols = int(input('Quantidade de gols'))
     print(f'O jogador {nome} fez {gols} gol(s) .')
 
 
-#programa principal
-nome = 'Lucas'#str(input('Nome do jogador:'))
-gols = int(input('Quantidade de gols:'))
-ficha('Lucas', 5)
+ficha('Neymar')
+
+ficha(gols=5)
+
+ficha('Neymar',2)
+
