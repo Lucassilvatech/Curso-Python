0729806c67c1d18c2c48fa499d0b376f5ff2954e
Lucas Silva <LucasSilvaCode@gmail.com>
2022-03-27 16:50:08 -0300

    master

--- Mundo-3/Exercicio/ex104.py
+++ Mundo-3/Exercicio/ex104.py
@@ -1,5 +1,18 @@
 '''Crie um programa que tenha uma função chamada leiaint(), que vai funcionar de forma semelhante á função input().
 So que agora fazendo a validação para aceitar somente valores numericos.'''
 
-def leiaint(num):
-    input
+def leiaint(msg):
+    ok = False
+    valor = 0
+    while True:
+        num = str(input(msg))
+        if num.isnumeric():
+            ok = True
+            valor = int(num)
+            return valor
+        else:
+            print('\033[0;31m[Erro!] Digite um numero inteiro valido.\033[m') 
+        if ok == True:
+            break
+n = leiaint('Digite um numero:')
+print(f'Você digitou o numero {n}')
